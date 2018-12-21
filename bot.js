dc598a5e959447918e15d32f35fed697031bb123
Muntzer23Pro <44648304+Muntzer23Pro@users.noreply.github.com>
2018-12-21 15:15:10 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523048543133630465")
+setInterval(function() {
+channel.send(`Wings Community Best Server``);
+}, 30)
+})
+
+("NTI1NjM2ODA3OTQ2MTQxNjk2.Dv5opQ.QMG5sA94fzSo3E26oAlkMj-kKI0"YBFJPmhAVGOWM");
